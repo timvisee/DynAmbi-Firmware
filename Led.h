475dd00ba1143f059441aa1d2c0436e152762736
Tim Visée <timvisee@gmail.com>
2016-05-01 17:51:06 +0200

    Update copyright notice in Led class

--- Led.h
+++ Led.h
@@ -1,13 +1,27 @@
-/**
- * HhsArduinoCircuitAdvanced.
- * An Arduino challenge for HHS by Tim Visee.
- *
- * @author Tim Visee
- * @website http://timvisee.com/
- */
+/******************************************************************************
+ * Copyright (c) ArduinoDynambi 2016. All rights reserved.                    *
+ *                                                                            *
+ * @author Tim Visee                                                          *
+ * @website http://timvisee.com/                                              *
+ *                                                                            *
+ * Open Source != No Copyright                                                *
+ *                                                                            *
+ * Permission is hereby granted, free of charge, to any person obtaining a    *
+ * copy of this software and associated documentation files (the "Software"), *
+ * to deal in the Software without restriction, including without limitation  *
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,   *
+ * and/or sell copies of the Software, and to permit persons to whom the      *
+ * Software is furnished to do so, subject to the following conditions:       *
+ *                                                                            *
+ * The above copyright notice and this permission notice shall be included    *
+ * in all copies or substantial portions of the Software.                     *
+ *                                                                            *
+ * You should have received a copy of The MIT License (MIT) along with this   *
+ * program. If not, see <http://opensource.org/licenses/MIT/>.                *
+ ******************************************************************************/
 
-#ifndef HHSARDUINOCIRCUITADVANCED_LED_H
-#define HHSARDUINOCIRCUITADVANCED_LED_H
+#ifndef DYNAMBI_LED_H
+#define DYNAMBI_LED_H
 
 #include <Arduino.h>
 
@@ -287,4 +301,4 @@ public:
     int calculatePulseDuration(int from, int to);
 };
 
-#endif //HHSARDUINOCIRCUITADVANCED_LED_H
+#endif //DYNAMBI_LED_H
